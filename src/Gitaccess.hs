36beb98905d581d82ae4316f6b74c7e04b7b9e7c
HughLav <HughLavery@Hughs-MacBook-Pro.local>
2017-12-08 19:00:26 +0000

    Can get list of commits for a repository for master and can clone repository and go to commit for slave.

--- src/Gitaccess.hs
+++ src/Gitaccess.hs
@@ -2,37 +2,62 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Gitaccess where 
 
 import Control.Monad
---import Data.Vector
 import GitHub
 import GitHub.Data
 import GitHub.Endpoints.Repos.Commits
-import Data.Text         (Text, pack)
+import Data.Text         (Text, pack, stripSuffix)
 import GitHub.Internal.Prelude
---import Data.Text.IO
 import Prelude
---import qualified GitHub.Repos.Commits as Github
+import Data.Char
+import System.FilePath
+import Data.Either
+import System.Directory
+import System.Process
+
+
+repository :: Name Repo
+repository = "Distributed-file-system"
 
 main :: IO ()
 main = do
-    getCommitList
-    --printCommits commits
+    shas <- getCommitList
+    putStrLn (show shas)
+    getRepo
+    getCommit "b865e244571d87de626258cb8f8fce3e54a4d70e"
+    putStrLn "got that commit"
 
 
-getCommitList :: IO ()
+getCommitList :: IO [Maybe Text] 
 getCommitList = do 
-    tmp <- commitsFor "tensorflow" "haskell" --FetchAll
+    tmp <- commitsFor "Hughlav" repository --FetchAll
     case tmp of
-        (Left error)    -> putStrLn $ "Error: " ++ (show error)
+        (Left error)    -> return [Nothing]
         (Right commits) -> do
-            let tmp = intercalate "\n" $ map formatCommit (toList commits)
-            putStrLn tmp
+            let tmp = map formatCommit (toList commits) --intercalate "\n" $
+            let shas = map parseString (tmp)
+            let tmper = map pack shas
+            let newShas = map (stripSuffix "\"")(tmper)
+            return newShas
     
 formatCommit :: GitHub.Commit -> String
 formatCommit commit =
    (show $ GitHub.commitSha commit)
 
+parseString :: [Char] -> [Char]
+parseString ('N':xs) = parseString xs
+parseString (' ':xs) = parseString xs
+parseString ('"':xs) = parseString xs
+parseString xs = xs   
 
+getRepo :: IO()
+getRepo = do
+    callProcess "git" ["clone", "https://github.com/Hughlav/Distributed-file-system.git"]
+
+getCommit :: String -> IO String
+getCommit commit = do
+    readCreateProcess ((proc "git" ["reset","--hard",commit]){ cwd = Just "Distributed-file-system"}) ""
